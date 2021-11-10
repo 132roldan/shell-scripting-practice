1e515d0fccc00dcbcff77e72d931c4a87cfa4d9b
Romualdo <132roldan@gmail.com>
2021-11-10 10:48:00 -0300

    paste code into calc

--- calc.sh
+++ calc.sh
@@ -1,11 +1,49 @@
-#! /usr/bin/env bash
 
-# When the program is first loaded, display a greeting to the user.
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
-# Then, capture the user selection.
-# If the selection matches a supported operation, execute the operation.
-# If the selection does not match a support operation, display an error message.
-# When the operation is complete, redisplay the menu.
+echo " try add or subtract two numbers:"
+echo " if do you wanna add type 'add'"
+echo " if do you wanna subtract, type 'sub'"
+read op
+case $op in
+	"add"|"+")
+		echo "Ok,adding some numbers"
+		fim=1
+while [ 1 -eq $fim ];
+do
+echo "type the first number and press enter"
+read fnumber
+echo "now, type the second number and press enter"
+read snumber
+let total=$fnumber+$snumber
+echo "result is $total"
+echo "do you wanna do more math?"
+echo " enter 1 for continue adding numbers or any number for exit"
+
+read fim
+done
+
+		;;
+	"subtract"|"s"|"sub")
+		echo "Ok, subtracting some number"
+		fim=1
+while [ 1 -eq $fim ];
+do
+echo "type the first number and press enter"
+read fnumber
+echo "now, type the second number and press enter"
+read snumber
+let total=$fnumber-$snumber
+echo "result is $total"
+echo "do you wanna do more math?"
+echo " enter 1 for continue subtracting numbers or any number for exit"
+
+read fim
+done
+
+		;;
+	*)
+		echo "whoop, I only know how to add or subtract." >&2
+		;;
+esac
+echo " Very impressive eh? enter 1 if you wanna go back or any number for exit"
+read fim
+done
